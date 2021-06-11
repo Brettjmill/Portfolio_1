277f5e53b5c634952a303e9b7966465bf7be78c4
Listerine87 <brettjmill@gmail.com>
2021-06-10 22:44:00 -0700

    Initial commit for Portfolio 1 assignment.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Portfolio_1.Controllers     //be sure to use your own project's namespace!
+{
+    public class HelloController : Controller   //remember inheritance??
+    {
+        //for each route this controller is to handle:
+        [HttpGet]       //type of request
+        [Route("")]     //associated route string (exclude the leading /)
+        public string Index()
+        {
+            return "This is my index!";
+        }
+
+        [HttpGet]       //type of request
+        [Route("projects")]     //associated route string (exclude the leading /)
+        public string Projects()
+        {
+            return "These are my projects!";
+        }
+
+        [HttpGet]       //type of request
+        [Route("contact")]     //associated route string (exclude the leading /)
+        public string Contact()
+        {
+            return "This is my Contact!";
+        }
+    }
+}
+
